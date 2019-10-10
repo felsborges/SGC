c42c9d4504fb85ef3c72add23a3a98d52196aad7
Felipe dos Santos Borges <felipeb@dbserver.com.br>
2019-10-10 13:47:05 -0300

    Implementação da interface IClienteRepository

---
+++ src/SGC.ApplicationCore/Intefaces/Repository/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using SGC.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Intefaces.Repository
+{
+    public interface IClienteRepository : IRepository<Cliente>
+    {
+        Cliente ObterPorProfissao(int clienteId);
+    }
+}
---
+++ src/SGC.Infrastructure/Repository/ClienteRepository.cs
@@ -0,0 +1,23 @@
+﻿using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Intefaces.Repository;
+using SGC.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SGC.Infrastructure.Repository
+{
+    public class ClienteRepository : EFRepository<Cliente>, IClienteRepository
+    {
+        public ClienteRepository(ClienteContext dbContext) : base(dbContext)
+        {
+
+        }
+        public Cliente ObterPorProfissao(int clienteId)
+        {
+            return Buscar(x => x.ProfissoesClientes.Any(p => p.ClienteId == clienteId))
+                .FirstOrDefault();
+        }
+    }
+}
